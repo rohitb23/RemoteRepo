108a790fee9fabdff404d2241f526f5f9e397034
Rohit <rbaweja2311@gmail.com>
2018-04-20 14:59:38 +0530

    Moving agaib
    Signed-off-by: Rohit <rbaweja2311@gmail.com>

---
+++ GitUp/src/Testing/Test1.java
@@ -0,0 +1,14 @@
+package Testing;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Go for it ");
+		System.out.println(Calendar.AM_PM);
+	}
+
+}
