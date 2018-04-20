905a423687d57961a381fcb6779fbe81aadd3db6
Rohit <rbaweja2311@gmail.com>
2018-04-20 14:55:57 +0530

    Moving to GIT
    Signed-off-by: Rohit <rbaweja2311@gmail.com>

---
+++ Learning/src/Learn/L.java
@@ -0,0 +1,24 @@
+package Learn;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class L {
+	
+	public static void main (String[] args) {
+		System.out.println("Welcome back");
+		int a = 23 ;
+		byte b = (byte) a;
+		System.out.println(b);
+		String[] r = { "Rohit" ,"Manisha" ,"RB" ,"MRB"};
+		int i = 1 ;
+		System.out.println(r.length);
+		for (String g : r) {
+			System.out.print(i + " : ");
+			System.out.println("Name is: " + g);
+			i++;
+			
+		System.out.println("We are moving to GIt");
+		}
+		}
+	}
